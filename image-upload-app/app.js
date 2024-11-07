bb7e7e7a55ab963ed0e7c32501468ab853f7fb64
FelixYifeiWang <yifei_wang2@brown.edu>
2024-11-07 03:58:58 -0500

    test

--- image-upload-app/app.js
+++ image-upload-app/app.js
@@ -32,7 +32,7 @@ app.use('/auth', authRoutes);       // Authentication routes
 app.use('/upload', uploadRoutes);   // Image upload routes
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/upload.html'));
+  res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
 // Start server
