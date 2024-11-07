6ad8efa4ec401bfbe7db2912c0fdab41f5b04478
FelixYifeiWang <yifei_wang2@brown.edu>
2024-11-07 04:06:22 -0500

    test

--- image-upload-app/app.js
+++ image-upload-app/app.js
@@ -36,7 +36,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 app.get('/', (req, isLoggedIn, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
+  res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 
 // Start server
