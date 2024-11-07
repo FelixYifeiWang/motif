ef73bbd599f584c0271aa0ec72d7b0ecfc44e3e6
Felix Wang <Felix.Wang@jjecn.com>
2024-11-07 08:22:29 -0500

    test

--- image-upload-app/config/passport.js
+++ image-upload-app/config/passport.js
@@ -11,6 +11,7 @@ passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
+const now = new Date();
 // Set up Google OAuth strategy
 passport.use(new GoogleStrategy({
   clientID: '274022818559-ea6ef22vv6li95t754epb48ug70hoh4m.apps.googleusercontent.com',
@@ -21,6 +22,18 @@ passport.use(new GoogleStrategy({
     google_id: profile.id,
     name: profile.displayName,
     email: profile.emails[0].value,
+    join_time: now.toISOString().split('T')[0],
+    styles: {
+      "casual": 0,
+      "edgy": 0,
+      "minimalist": 0,
+      "sophisticated": 0,
+      "athleisure": 0,
+      "romantic": 0,
+      "bohemian": 0,
+      "street": 0,
+      "glam": 0
+    }
   };
 
   // Save user to DynamoDB if new
